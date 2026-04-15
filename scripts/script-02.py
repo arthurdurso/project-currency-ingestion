e08f1ebac7c4ac6e7a92b8db451d566a8cd0d311
Durso <dursoarthur@gmail.com>
2026-04-15 00:49:40 -0300

    chore(scripts): testing changes

--- scripts/script-02.py
+++ scripts/script-02.py
@@ -1,12 +1,17 @@
 # Pipeline script for ingesting from FreeCurrency API using DLT
 
-
 import dlt
+from dlt.destinations import filesystem
 from ingestion.source import freecurrency_source
+from pathlib import Path
 
+
+local_destination = filesystem(
+        bucket_url=Path("local_data").resolve().as_uri()
+    )
 pipeline = dlt.pipeline(
     pipeline_name="freecurrency_pipeline",
-    destination="filesystem",   # salva em disco local
+    destination=local_destination,   # salva em disco local
     dataset_name="latest",
 )
 
